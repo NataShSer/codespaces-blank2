707e56680778b0a44d868e31e9f24f36b3eef723
NataShSer <96727318+NataShSer@users.noreply.github.com>
2022-12-20 10:05:03 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <BarComponent v-if="dataset.length > 0" :dataset="dataset" />
+    <PieChart v-if="dataset.length > 0" :dataset="dataset" />
+  </div>
+</template>
+
+<script>
+import BarComponent from './components/BarComponent.vue'
+import PieChart from './components/PieChart.vue'
+export default {
+  name: 'App',
+  components: {
+    BarComponent,
+    PieChart
+  },
+  data() {
+    return {
+      dataset: []
+    }
+  },
+  created() {
+    this.axios.get('data.json').then(response => {
+      this.dataset = response.data
+    })
+  }
+}
+</script>
