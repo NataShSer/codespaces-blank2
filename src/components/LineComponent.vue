c497e0bfd4c67afb43d96763ada4417626bd8318
NataShSer <96727318+NataShSer@users.noreply.github.com>
2023-01-05 00:11:25 +0300

    Create LineComponent.vue

---
+++ src/components/LineComponent.vue
@@ -0,0 +1,106 @@
+<template>
+    <LineChartGenerator
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+  </template>
+  
+  <script>
+  import { Line as LineChartGenerator} from 'vue-chartjs/legacy'
+  
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    CategoryScale,
+    PointElement
+  } from 'chart.js'
+  
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    CategoryScale,
+    PointElement
+  )
+  
+  export default {
+    name: 'LineChart',
+    components: {
+        LineChartGenerator
+    },
+    props: {
+      chartId: {
+        type: String,
+        default: 'line-chart'
+      },
+      datasetIdKey: {
+        type: String,
+        default: 'label'
+      },
+      width: {
+        type: Number,
+        default: 400
+      },
+      height: {
+        type: Number,
+        default: 400
+      },
+      cssClasses: {
+        default: '',
+        type: String
+      },
+      styles: {
+        type: Object,
+        default: () => {}
+      },
+      plugins: {
+        type: Array,
+        default: () => []
+      },
+      dataset: Array
+    },
+    data() {
+      return {
+        chartData: {
+          labels: ['A', 'B', 'C', 'D'],
+          datasets: [
+            {
+              label: 'Студенты',
+              backgroundColor: '#f87979',
+              data: [0, 0, 0, 0]
+            }
+          ]
+        },
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    created() {
+        this.dataset.forEach(student => {
+        if (student.race.ethnicity == "group A")
+            this.chartData.datasets[0].data[0]++;
+        else if (student.race.ethnicity == "group B")
+            this.chartData.datasets[0].data[1]++;
+        else if (student.race.ethnicity == "group C")
+            this.chartData.datasets[0].data[2]++;
+        else
+            this.chartData.datasets[0].data[3]++;
+        });
+    }
+  }
+  </script>
